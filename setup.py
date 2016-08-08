3e183fa45dc33053f8f51941d3430d89f4146d9d
Hiroki Danno <redgrapefruit@mac.com>
2016-08-08 10:03:40 +0900

    add setup and __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Primo",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Hiroki Danno",
+    author_email="redgrapefruit@mac.com",
+    description="Analyzing single-cell RNA-seq data",
+    license="MIT",
+    keyword="single-cell RNA-seq RNAseq spatial reconstruction WISH"
+)
