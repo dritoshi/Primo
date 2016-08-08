cd9fa2732790edad6e069f0e49bb4b13d5fb0a68
Hiroki Danno <redgrapefruit@mac.com>
2016-08-08 17:57:22 +0900

    add cv plots

--- primo/primo.py
+++ primo/primo.py
@@ -1,10 +1,7 @@
 from __future__ import print_function
 
-# import os
+import os
 # import glob
-
-# import matplotlib.pyplot as plt
-# import seaborn as sns
 
 import pandas as pd
 import numpy as np
@@ -15,6 +12,11 @@ from sklearn.preprocessing import scale
 # from sklearn.manifold import TSNE
 # from sklearn.decomposition import PCA, RandomizedPCA
 # from sklearn.cluster import AggromerativeClustering
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+# import seaborn as sns
 
 
 class Primo(object):
@@ -183,7 +185,7 @@ class Primo(object):
 
         return self
 
-    def filter_variable_genes(self, z_cutoff, max_count=5):
+    def filter_variable_genes(self, z_cutoff, max_count=5, bin_num=200):
         """Filters genes which coefficient of variation are high.
 
         Parameters
@@ -204,22 +206,69 @@ class Primo(object):
         ind = self.df_rnaseq.max(axis=1) > max_count
         df_log = np.log(self.df_rnaseq.ix[ind, :] + 0.1)
 
-        bin_num = 2000
         bin_label = ["bin" + str(i) for i in range(1, bin_num + 1)]
         df_log['bin'] = pd.qcut(df_log.mean(axis=1), bin_num, labels=bin_label)
 
-        variable_genes = []
+        self.variable_genes = []
         for binname in bin_label:
             df_log_bin = df_log[df_log.bin == binname].ix[
                 :, :df_log.shape[1]-1]
             dispersion_measure = (df_log_bin.var(axis=1) /
                                   df_log_bin.mean(axis=1))
-            variable_genes_bin = df_log_bin.index[
-                scale(dispersion_measure) > z_cutoff].values
-            variable_genes.extend(variable_genes_bin)
+            z_disp = ((dispersion_measure - dispersion_measure.mean()) /
+                      dispersion_measure.std())
 
-        self.df_rnaseq_variable = self.df_rnaseq.ix[variable_genes, :]
+            variable_genes_bin = df_log_bin.index[z_disp > z_cutoff].values
+            self.variable_genes.extend(variable_genes_bin)
+
+        self.df_rnaseq_variable = self.df_rnaseq.ix[self.variable_genes, :]
         self.num_genes_variable = self.df_rnaseq_variable.shape[0]
+
+        return self
+
+    def plot_cv(self, output_dir, colorize_variable_genes=True):
+        """Plot Mean-CV plot
+
+        Parameters
+        ----------
+        output_dir : str
+            path of output directory
+        colorize_variable_genes : bool
+            If `True`, colorize variable genes
+
+        Return
+        ------
+        self : object
+            Returns the instance itself
+
+        """
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+
+        output_file = os.path.join(output_dir, "mean_cv_plot.png")
+
+        if colorize_variable_genes is True:
+            ind = self.df_rnaseq.index.isin(self.df_rnaseq_variable.index)
+
+        mean = self.df_rnaseq.mean(axis=1)
+        std = self.df_rnaseq.std(axis=1)
+        cv2 = (std / mean) ** 2
+        cv2_poisson = 1. / mean
+
+        fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+        ax.scatter(mean, cv2, c="lightgray", marker=".", edgecolors="none")
+        ax.scatter(mean, cv2_poisson, c="green", marker=".", edgecolors="none")
+        if colorize_variable_genes:
+            ax.scatter(mean[ind], cv2[ind], c="magenta",
+                       marker=".", edgecolors="none")
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.set_xlim(10 ** -2, 10 ** 2.5)
+        ax.set_ylim(10 ** -2, 10 ** 2.5)
+        ax.set_xlabel("Mean")
+        ax.set_ylabel("CV ** 2")
+
+        plt.savefig(output_file)
 
         return self
 
@@ -241,6 +290,7 @@ class Primo(object):
 
 
 if __name__ == '__main__':
+    pass
 
     p = Primo()
     (p.load_scRNAseq_data("../data/St13_1st_dge.txt.gz").
@@ -248,4 +298,5 @@ if __name__ == '__main__':
      remove_outlier_cells(2.0).
      normalize())
 
-    p.filter_variable_genes(z_cutoff=1.7, max_count=5)
+    p.filter_variable_genes(z_cutoff=1.3, max_count=5)
+    p.plot_cv("../results")
