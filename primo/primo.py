055e711c0bb602f8b779575fee39ee4df2f2073f
Hiroki Danno <redgrapefruit@mac.com>
2016-08-08 14:52:55 +0900

    define Primo class in primo.py

--- primo/primo.py
+++ primo/primo.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # # from scipy.stats import xxx
 
-# from sklearn.preprocessing import scale
+from sklearn.preprocessing import scale
 # from sklearn.manifold import TSNE
 # from sklearn.decomposition import PCA, RandomizedPCA
 # from sklearn.cluster import AggromerativeClustering
@@ -66,12 +66,60 @@ class Primo(object):
         else:
             self.df_rnaseq = path_or_dataframe
 
+        self._remove_all_zero()
+        self._update_info()
+
+        return self
+
+    def remove_gene_maxlessthan(self, max_count):
+        """
+        (Deprecated)
+        Remove genes of which expression max is less than `max_count`.
+
+        Parameters
+        ----------
+        max_count : float
+            Genes of which expression max is less than `max_count` are
+            filterd out.
+
+        Return
+        ------
+        self : object
+            Returns the instance itself
+
+        """
+
+        ind = self.df_rnaseq.max(axis=1) > max_count
+        self.df_rnaseq = self.df_rnaseq.ix[ind, :]
+        self._remove_all_zero()
+        self._update_info()
+
+        return self
+
+    def remove_gene_toohigh(self, cutoff):
+        """
+        Remove genes of which expression mean is highre than `cutoff`.
+
+        Parameters
+        ----------
+        cutoff : float
+            Genes of which expression mean is higher than `cutoff`
+            are filtered out.
+
+        Return
+        ------
+        self : object
+            Returns the instance itself
+
+        """
+        ind = self.df_rnaseq.mean(axis=1) < cutoff
+        self.df_rnaseq = self.df_rnaseq.ix[ind, :]
+        self._remove_all_zero()
         self._update_info()
 
         return self
 
     def remove_outlier_cells(self, val):
-
         """Remove outlier cells
 
         Parameters
@@ -93,6 +141,7 @@ class Primo(object):
         index_not_outlier = abs(self.df_rnaseq.sum() - mean) < (val * sd)
 
         self.df_rnaseq = self.df_rnaseq.ix[:, index_not_outlier]
+        self._remove_all_zero()
         self._update_info()
 
         return self
@@ -117,38 +166,77 @@ class Primo(object):
         if normalize_factor is None:
             normalize_factor = self.df_rnaseq.sum().mean()
 
-        self.df_rnaseq = (1.0 * normalize_factor * self.df_rnaseq
-                          / self.df_rnaseq.sum())
+        self.df_rnaseq = (1.0 * normalize_factor *
+                          self.df_rnaseq / self.df_rnaseq.sum())
 
+        self._remove_all_zero()
         self._update_info()
 
         return self
 
-    def filter_variable_genes(self, z):
+    def filter_variable_genes(self, z_cutoff, max_count=5):
         """Filters genes which coefficient of variation are high.
 
         Parameters
         ----------
-        z : float
+        z_cutoff : float
             z score for filtering
+
+        max_count : float
+            Genes of which expression max is less than `max_count` are
+            filterd out.
 
         Return
         ------
         self : object
             Returns the instance itself
         """
-        df_log = np.log(self.df_rnaseq + 0.1)
+
+        ind = self.df_rnaseq.max(axis=1) > max_count
+        df_log = np.log(self.df_rnaseq.ix[ind, :] + 0.1)
+
+        bin_num = 2000
+        bin_label = ["bin" + str(i) for i in range(1, bin_num + 1)]
+        df_log['bin'] = pd.qcut(df_log.mean(axis=1), bin_num, labels=bin_label)
+
+        variable_genes = []
+        for binname in bin_label:
+            df_log_bin = df_log[df_log.bin == binname].ix[
+                :, :df_log.shape[1]-1]
+            dispersion_measure = (df_log_bin.var(axis=1) /
+                                  df_log_bin.mean(axis=1))
+            variable_genes_bin = df_log_bin.index[
+                scale(dispersion_measure) > z_cutoff].values
+            variable_genes.extend(variable_genes_bin)
+
+        self.df_rnaseq_variable = self.df_rnaseq.ix[variable_genes, :]
+        self.num_genes_variable = self.df_rnaseq_variable.shape[0]
+
+        return self
+
+    def _remove_all_zero(self):
+        genes_not_all_zero = (self.df_rnaseq.sum(axis=1) != 0)
+        cells_not_all_zero = (self.df_rnaseq.sum(axis=0) != 0)
+        self.df_rnaseq = self.df_rnaseq.ix[genes_not_all_zero,
+                                           cells_not_all_zero]
+
+        return self
 
     def _update_info(self):
-
         self.genes = list(self.df_rnaseq.index)
+        self.num_genes = len(self.genes)
         self.cells = list(self.df_rnaseq.columns)
+        self.num_cells = len(self.cells)
 
         return self
 
 
 if __name__ == '__main__':
+
     p = Primo()
-    (p.load_scRNAseq_data("../data/test.txt").
+    (p.load_scRNAseq_data("../data/St13_1st_dge.txt.gz").
+     remove_gene_toohigh(500).
      remove_outlier_cells(2.0).
      normalize())
+
+    p.filter_variable_genes(z_cutoff=1.7, max_count=5)
