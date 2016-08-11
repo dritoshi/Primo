e373ea1813b1e252d802df996a0f91c8c2427b48
Hiroki Danno <redgrapefruit@mac.com>
2016-08-11 11:19:48 +0900

    add output_dir

--- example/example1.py
+++ example/example1.py
@@ -8,13 +8,16 @@ if __name__ == '__main__':
 
     from primo import Primo
 
+    output_dir = "../results"
+
     p = Primo()
     (p.load_scRNAseq_data("../data/St13_1st_dge.txt.gz",
                           num_stamp=1200).
      remove_gene_toohigh(500).
      remove_outlier_cells(2.0).
      normalize().
-     filter_variable_genes(z_cutoff=1.2, max_count=5,
+     filter_variable_genes(z_cutoff=1.1, max_count=3,
                            bin_num=2000, stack=True).
-     plot_cv("../results/")
+     plot_cv(output_dir).
+     tsne(plot=True, output_dir=output_dir, init="pca", random_state=12345)
      )
