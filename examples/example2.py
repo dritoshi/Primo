a0c37eeb805284cf0b696e8f5806c8d66d6939ea
Hiroki Danno <redgrapefruit@mac.com>
2016-08-17 13:06:45 +0900

    make example2.py as small test

--- examples/example2.py
+++ examples/example2.py
@@ -9,16 +9,16 @@ if __name__ == '__main__':
     from primo.position import Position
     from primo.spatial_expression import SpatialExpression
 
-    output_dir = "./results"
+    output_dir = "./results_example2"
 
     r = (RNAseq().
          load_scRNAseq_data("./data/St13_1st_dge.txt.gz",
-                            num_stamp=1200,
+                            num_stamp=300,
                             annotation_type="uid").
          remove_gene_toohigh(500).
          remove_outlier_cells(2.0).
          normalize().
-         filter_variable_genes(z_cutoff=1.1, max_count=3,
+         filter_variable_genes(z_cutoff=1.2, max_count=3,
                                bin_num=2000, stack=True).
          plot_cv(output_dir).
          tsne(plot=True, output_dir=output_dir, init="pca", random_state=12345)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
          load_WISH_images("./data/wish",
                           annotation_type="symbol").
          symbol_to_uid("./data/uid_symbol.tsv").
-         filter_images(pixel=80).
+         filter_images(pixel=30).
          plot_wish(output_dir)
          )
 
